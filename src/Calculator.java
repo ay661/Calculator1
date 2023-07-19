c81b316ab4cb733c87e18109aa98d8139d9ae784
Artem <artem.yakushev25@gmail.com>
2023-07-20 05:09:51 +0700

    rg

---
+++ src/Calculator.java
@@ -0,0 +1,141 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Calculator {
+    public static void main(String[] args) {
+        String enter, num1, num2;
+        int result=0;
+        String [] romeNumbers = new String[10];
+
+        romeNumbers[0] = "I";
+        romeNumbers[1] = "II";
+        romeNumbers[2] = "III";
+        romeNumbers[3] = "IV";
+        romeNumbers[4] = "V";
+        romeNumbers[5] = "VI";
+        romeNumbers[6] = "VII";
+        romeNumbers[7] = "VIII";
+        romeNumbers[8] = "IX";
+        romeNumbers[9] = "X";
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите арифметическую операцию:");
+        //ввод данных
+        enter = in.nextLine();
+        enter = enter.toUpperCase();
+        System.out.println("Вы написали: " + enter);
+        String[] strings = enter.split(" ");
+        int a, b;
+        num1 = strings[0];
+        num2 = strings[2];
+        char x=strings[1].charAt(0);
+
+        if (Arrays.asList(romeNumbers).contains(num1) && Arrays.asList(romeNumbers).contains(num2)){
+            a = toArab1(num1);
+            b = toArab2(num2);
+            if (a>b) {
+                switch (x) {
+                    case '+':
+                        result = a + b;
+                        break;
+                    case '-':
+                        result = a - b;
+                        break;
+                    case '*':
+                        result = a * b;
+                        break;
+                    case '/':
+                        result = a / b;
+                        break;
+                    default:
+                        result = 0;
+                        System.out.println("Ошибка");
+                }
+
+            }
+            else {
+                System.out.println("Ошибка");
+            }
+
+        }
+        else {
+            a = Integer.parseInt(strings[0]);
+            b = Integer.parseInt(strings[2]);
+            if (a>0 && a<11 && b>0 && b<11) {
+                switch (x) {
+                    case '+':
+                        result = a + b;
+                        break;
+                    case '-':
+                        result = a - b;
+                        break;
+                    case '*':
+                        result = a * b;
+                        break;
+                    case '/':
+                        result = a / b;
+                        break;
+                    default:
+                        result = 0;
+                        System.out.println("Ошибка");
+                }
+            }
+            else {
+                System.out.println("Ошибка");
+            }
+        }
+        System.out.println("Результат: "+ result);
+    }
+
+    private static int toArab1(String num1) {
+        switch (num1) {
+            case "I":
+                return 1;
+            case "II":
+                return 2;
+            case "III":
+                return 3;
+            case "IV":
+                return 4;
+            case "V":
+                return 5;
+            case "VI":
+                return 6;
+            case "VII":
+                return 7;
+            case "VIII":
+                return 8;
+            case "IX":
+                return 9;
+            case "X":
+                return 10;
+        }
+        return 0;
+    }
+    private static int toArab2(String num2) {
+        switch (num2) {
+            case "I":
+                return 1;
+            case "II":
+                return 2;
+            case "III":
+                return 3;
+            case "IV":
+                return 4;
+            case "V":
+                return 5;
+            case "VI":
+                return 6;
+            case "VII":
+                return 7;
+            case "VIII":
+                return 8;
+            case "IX":
+                return 9;
+            case "X":
+                return 10;
+        }
+        return 0;
+    }
+}
